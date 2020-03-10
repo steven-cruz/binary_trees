ab9c9364d8f6470151d0fcfaf9068819fe13eae6
steven-cruz <stevencruz.300008@gmail.com>
2020-03-10 17:35:34 +0000

    the structure was fixed

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef BINARY_TRESS_H
 #define BINARY_TRESS_H
 
+#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,25 +24,6 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
 /* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
 
@@ -51,6 +33,8 @@ typedef struct binary_tree_s avl_t;
 /* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
+/* function that prints the tree */
+void binary_tree_print(const binary_tree_t *);
 
 /* Prototype Functions of all tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
