b22747c00d04f67ecd75fa224c90b5b1729aa5af
steven-cruz <stevencruz.300008@gmail.com>
2020-03-11 19:44:10 +0000

    goes through a binary tree using pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal.
+ * @tree: is the tree to walk.
+ * @func: receives the parameter value.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
