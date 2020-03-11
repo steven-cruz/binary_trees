3e46c73300082025e3e2808d3dc03c4927f10f10
steven-cruz <stevencruz.300008@gmail.com>
2020-03-11 17:19:18 +0000

    checks if a given node is a root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,7 +10,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->left != NULL && node->right != NULL)
+	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
