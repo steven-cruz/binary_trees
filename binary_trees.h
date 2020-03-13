477c2e3ab0d33765b62642e291c79828496006b2
steven-cruz <stevencruz.300008@gmail.com>
2020-03-13 19:46:54 +0000

    added prototype of task number 103

--- binary_trees.h
+++ binary_trees.h
@@ -57,5 +57,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+/* tasks advanced */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
